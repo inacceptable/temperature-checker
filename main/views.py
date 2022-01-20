91d00cbab454a11f6d001a58e4ae3d96af45aa0b
Con <62197403+inacceptable@users.noreply.github.com>
2022-01-20 14:15:11 +1100

    new

---
+++ main/views.py
@@ -0,0 +1,19 @@
+import requests
+from django.conf import settings
+from django.shortcuts import render
+from django.http import JsonResponse
+def index(request):
+	apidata = requests.get('https://api.openweathermap.org/data/2.5/weather?q=melbourne,3000,australia&appid=df584a0437488bfbfafc54efe54ddced&units=metric').json()
+	context = {
+		'apidata' : apidata,
+	}
+	return render(request, 'html/index.html', context)
+
+def search(request): 
+	city = str(request.GET.get('city'))
+	postcode = str(request.GET.get('postcode'))
+	country = str(request.GET.get('country'))
+	apilink = "https://api.openweathermap.org/data/2.5/weather?q="+city+","+postcode+","+country+"&appid=df584a0437488bfbfafc54efe54ddced&units=metric"
+	apidata = requests.get(apilink).json()
+	data = {'apidata' : apidata}
+	return JsonResponse(data)
